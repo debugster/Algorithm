062409660f89d9642fdde6e4c1a14e1b60ffef18
Mahmood Nayeem <com.debugster@gmail.com>
2017-08-20 14:05:22 +0600

    Update BinarySearchTree.cpp

--- Tree/BinarySearchTree.cpp
+++ Tree/BinarySearchTree.cpp
@@ -17,6 +17,7 @@ void preOrder(node *p);
 void postOrder(node *p);
 void menu();
 void display();
+bool searchData(int value);
 
 int main()
 {
@@ -41,13 +42,24 @@ int main()
                 break;
 
             case 3:
+                printf("Enter a value : ");
+                scanf("%d", &n);
+                if (searchData(n)) {
+                    printf("%d was found.\n", n);
+                }
+                else {
+                    printf("%d was not found.\n", n);
+                }
+                break;
+
+            case 4:
                 printf("Exiting. Thank you.\n");
                 break;
 
             default:
                 printf("Invalid choice. Try Again.\n");
         }
-    } while (choice != 3);
+    } while (choice != 4);
 
     return 0;
 }
@@ -117,7 +129,8 @@ void menu()
     printf("\nMENU:\n");
     printf("1. Insert data\n");
     printf("2. Display data\n");
-    printf("3. Exit\n");
+    printf("3. Search data\n");
+    printf("4. Exit\n");
     printf("Enter your choice : ");
 }
 
@@ -134,4 +147,30 @@ void display()
     else {
         printf("\nTREE is empty.\n");
     }
+}
+
+bool searchData(int value)
+{
+    node *p;
+    bool found = false;
+
+    p = root;
+    while (true) {
+        if (p == NULL) {
+            break;
+        }
+
+        if (p->info == value) {
+            found = true;
+            break;
+        }
+        else if (value < p->info) {
+            p = p->left;
+        }
+        else {
+            p = p->right;
+        }
+    }
+
+    return found;
 }
