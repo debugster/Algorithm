2cbb3783ca68a749232dc3369ef749b03c3d5bb7
Mahmood Nayeem <com.debugster@gmail.com>
2017-08-20 14:03:44 +0600

    Create BinarySearchTree.cpp

---
+++ Tree/BinarySearchTree.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node
+{
+    int info;
+    node *left;
+    node *right;
+};
+
+node *root;
+
+void insertNode(int item);
+void inOrder(node *p);
+void preOrder(node *p);
+void postOrder(node *p);
+void menu();
+void display();
+
+int main()
+{
+    node *p;
+    int n, choice;
+
+    root = NULL;
+
+    do {
+        menu();
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter a value : ");
+                scanf("%d", &n);
+                insertNode(n);
+                break;
+
+            case 2:
+                display();
+                break;
+
+            case 3:
+                printf("Exiting. Thank you.\n");
+                break;
+
+            default:
+                printf("Invalid choice. Try Again.\n");
+        }
+    } while (choice != 3);
+
+    return 0;
+}
+
+void insertNode(int item)
+{
+    node *p, *newNode, *temp;
+
+    newNode = (node *) malloc(sizeof(node));
+    newNode->info = item;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    if (root == NULL) {
+        root = newNode;
+        return;
+    }
+
+    p = root;
+    while (p != NULL) {
+        temp = p;
+        if (p->info > item) {
+            p = p->left;
+        }
+        else {
+            p = p->right;
+        }
+    }
+
+    if (temp->info > item) {
+        temp->left = newNode;
+    }
+    else {
+        temp->right = newNode;
+    }
+}
+
+void inOrder(node *p)
+{
+    if (p != NULL) {
+        inOrder(p->left);
+        printf("%d ", p->info);
+        inOrder(p->right);
+    }
+}
+
+void preOrder(node *p)
+{
+    if (p != NULL) {
+        printf("%d ", p->info);
+        preOrder(p->left);
+        preOrder(p->right);
+    }
+}
+
+void postOrder(node *p)
+{
+    if (p != NULL) {
+        postOrder(p->left);
+        postOrder(p->right);
+        printf("%d ", p->info);
+    }
+}
+
+void menu()
+{
+    printf("\nMENU:\n");
+    printf("1. Insert data\n");
+    printf("2. Display data\n");
+    printf("3. Exit\n");
+    printf("Enter your choice : ");
+}
+
+void display()
+{
+    if (root != NULL) {
+        printf("\nTraversing Tree INorder\n");
+        inOrder(root);
+        printf("\nTraversing Tree PREorder\n");
+        preOrder(root);
+        printf("\nTraversing Tree POSTorder\n");
+        postOrder(root);
+    }
+    else {
+        printf("\nTREE is empty.\n");
+    }
+}
